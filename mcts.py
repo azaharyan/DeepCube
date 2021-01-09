976b7002d64dc59582e25d85ca935f36ef60c8db
Stoyan Genchev <genchev_stoyan@abv.bg>
2021-01-09 17:42:31 +0200

    explore random unvisited child if all children haven't been visited, otherwise stick to most promising

--- mcts.py
+++ mcts.py
@@ -1,9 +1,8 @@
 import math
+import random
 from collections import deque
 
 import numpy as np
-
-from cubeEnv import CubeEnv
 
 loss_constant = 100
 exploration_constant = 100
@@ -42,45 +41,26 @@ class MCTS:
             if current not in self.children or not self.children[current]:
                 return path_to_leaf, actions_to_leaf, current
             else:
-                not_explored_children = [child for child in self.children[current] if child not in self.children]
-                if not_explored_children:
-                    first_not_explored_child = not_explored_children[0]
-                    action_index_to_first_not_explored_child = self.children[current].index(first_not_explored_child)
-                    path_to_leaf.append(current)
-                    actions_to_leaf.append(action_index_to_first_not_explored_child)
-                    return path_to_leaf, actions_to_leaf, first_not_explored_child
+                if sum(self.number_of_visits[current]) == 0:
+                    action_index = random.randint(0, len(current.get_action_space()) - 1)
                 else:
                     action_index = self.get_most_promising_action_index(current)
-                    path_to_leaf.append(current)
-                    actions_to_leaf.append(action_index)
-                    current = self.children[current][action_index]
+
+                path_to_leaf.append(current)
+                actions_to_leaf.append(action_index)
+                current = self.children[current][action_index]
 
     def expand(self, state):
         value, policy = self.model.predict(np.array(state.get_one_hot_state()).flatten()[None, :])
-        if state in self.children:
-            return value[0][0]
 
-        self.initialize_state_data(state, policy)
+        self.probabilities[state] = policy[0]
+        self.loss[state] = [0] * len(state.get_action_space())
+        self.score[state] = [0] * len(state.get_action_space())
+        self.number_of_visits[state] = [0] * len(state.get_action_space())
+
         self.children[state] = state.get_direct_children_if_not_solved()
 
-        for child in self.children[state]:
-            self.initialize_state_data(child)
-
         return value[0][0]
-
-    def initialize_state_data(self, state, policy=None):  # allow for providing policy to avoid predicting twice
-        if state not in self.score:  # doesn't matter which of score, probabilities or number_of_visits is used here
-            if policy is None:
-                _, policy = self.model.predict(np.array(state.get_one_hot_state()).flatten()[None, :])
-            self.score[state] = {}
-            self.probabilities[state] = {}
-            self.number_of_visits[state] = {}
-            self.loss[state] = {}
-            for i in range(len(state.get_action_space())):
-                self.score[state][i] = 0
-                self.probabilities[state][i] = policy[0][i]
-                self.number_of_visits[state][i] = 0
-                self.loss[state][i] = 0
 
     def backpropagate(self, path_to_leaf, actions_to_leaf, reward):
         for state_to_leaf, action_to_leaf in zip(path_to_leaf, actions_to_leaf):
@@ -89,11 +69,9 @@ class MCTS:
             self.loss[state_to_leaf][action_to_leaf] += loss_constant
 
     def get_most_promising_action_index(self, state):
-        state_all_actions_number_of_visits = 0
         action_space_len = len(state.get_action_space())
-        for i in range(action_space_len):
-            state_all_actions_number_of_visits += self.number_of_visits[state][i]
 
+        state_all_actions_number_of_visits = sum(self.number_of_visits[state])
         u_plus_w_a = [0] * action_space_len
         for i in range(action_space_len):
             u_st_a = exploration_constant \
@@ -102,18 +80,6 @@ class MCTS:
             u_plus_w_a[i] = u_st_a + self.score[state][i] - self.loss[state][i]
 
         return max(range(len(u_plus_w_a)), key=u_plus_w_a.__getitem__)
-
-    def play(self, state):
-        if state.is_solved():
-            raise Exception
-
-        cube = CubeEnv(cube=state.cube.copy())  # TODO: is shallow copy enough?
-        if state not in self.children:
-            cube.scramble()
-        else:
-            cube.step(self.get_most_promising_action_index(state))
-
-        return cube
 
     """
     Leaving here as I wrote it for testing to make sure bfs works
