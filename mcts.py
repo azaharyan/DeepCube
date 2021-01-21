7969d49a33cce45623fd5287b015c4791c7fe04a
Stoyan Genchev <genchev_stoyan@abv.bg>
2021-01-22 00:58:02 +0200

    use one map instead of 5 with same keys

--- mcts.py
+++ mcts.py
@@ -7,13 +7,17 @@ import numpy as np
 
 class MCTS:
 
-    def __init__(self, model, loss_constant = 150, exploration_constant = 4):
+    def __init__(self, model, loss_constant=150, exploration_constant=4):
         self.model = model
-        self.children = dict()
-        self.score = dict()
-        self.number_of_visits = dict()
-        self.probabilities = dict()
-        self.loss = dict()
+
+        self.children_and_data = dict()
+
+        self.ch_i = 0
+        self.p_i = 1
+        self.s_i = 2
+        self.n_of_v_i = 3
+        self.v_l_i = 4
+
         self.loss_constant = loss_constant
         self.exploration_constant = exploration_constant
 
@@ -23,7 +27,7 @@ class MCTS:
         self.backpropagate(path_to_leaf, actions_to_leaf, reward)
 
         i = 0
-        for s in self.children[leaf]:
+        for s in self.children_and_data[leaf][self.ch_i]:
             if s.is_solved():
                 actions_to_leaf.append(i)
                 actions_to_leaf_bfs = self.bfs(state)
@@ -37,46 +41,52 @@ class MCTS:
         actions_to_leaf = []
         current = state
         while True:
-            if current not in self.children or not self.children[current]:
+            if current not in self.children_and_data or not self.children_and_data[current][self.ch_i]:
                 return path_to_leaf, actions_to_leaf, current
             else:
-                if sum(self.number_of_visits[current]) == 0:
+                if sum(self.children_and_data[current][self.n_of_v_i]) == 0:
                     action_index = random.randint(0, len(current.get_action_space()) - 1)
                 else:
                     action_index = self.get_most_promising_action_index(current)
 
                 path_to_leaf.append(current)
                 actions_to_leaf.append(action_index)
-                self.loss[current][action_index] += self.loss_constant
-                current = self.children[current][action_index]
+                self.children_and_data[current][self.v_l_i][action_index] += self.loss_constant
+
+                current = self.children_and_data[current][self.ch_i][action_index]
 
     def expand(self, state):
         value, policy = self.model.predict(np.array(state.get_one_hot_state()).flatten()[None, :])
 
-        self.probabilities[state] = policy[0]
-        self.loss[state] = [0] * len(state.get_action_space())
-        self.score[state] = [0] * len(state.get_action_space())
-        self.number_of_visits[state] = [0] * len(state.get_action_space())
-
-        self.children[state] = state.get_direct_children_if_not_solved()
+        self.children_and_data[state] = (
+            state.get_direct_children_if_not_solved(),
+            policy[0],
+            [0] * len(state.get_action_space()),
+            [0] * len(state.get_action_space()),
+            [0] * len(state.get_action_space()))
 
         return value[0][0]
 
     def backpropagate(self, path_to_leaf, actions_to_leaf, reward):
         for state_to_leaf, action_to_leaf in zip(path_to_leaf, actions_to_leaf):
-            self.score[state_to_leaf][action_to_leaf] = max(self.score[state_to_leaf][action_to_leaf], reward)
-            self.number_of_visits[state_to_leaf][action_to_leaf] += 1
+            self.children_and_data[state_to_leaf][self.s_i][action_to_leaf] = max(
+                self.children_and_data[state_to_leaf][self.s_i][action_to_leaf], reward)
+
+            self.children_and_data[state_to_leaf][self.n_of_v_i][action_to_leaf] += 1
 
     def get_most_promising_action_index(self, state):
         action_space_len = len(state.get_action_space())
 
-        state_all_actions_number_of_visits = sum(self.number_of_visits[state])
+        state_all_actions_number_of_visits = sum(self.children_and_data[state][self.n_of_v_i])
         u_plus_w_a = [0] * action_space_len
         for i in range(action_space_len):
             u_st_a = self.exploration_constant \
-                     * self.probabilities[state][i] \
-                     * (math.sqrt(state_all_actions_number_of_visits) / (1 + self.number_of_visits[state][i]))
-            u_plus_w_a[i] = u_st_a + self.score[state][i] - self.loss[state][i]
+                     * self.children_and_data[state][self.p_i][i] \
+                     * (math.sqrt(state_all_actions_number_of_visits)
+                        / (1 + self.children_and_data[state][self.n_of_v_i][i]))
+            u_plus_w_a[i] = u_st_a \
+                            + self.children_and_data[state][self.s_i][i] \
+                            - self.children_and_data[state][self.v_l_i][i]
 
         return max(range(len(u_plus_w_a)), key=u_plus_w_a.__getitem__)
 
@@ -115,11 +125,11 @@ class MCTS:
                 solved = current
                 break
 
-            if current not in self.children:  # in MCTS not all branches are visited
+            if current not in self.children_and_data:  # in MCTS not all branches are visited
                 continue
 
             i = 0
-            for current_child in self.children[current]:
+            for current_child in self.children_and_data[current][self.ch_i]:
                 if current_child not in visited:
                     queue.append(current_child)
                     state_to_parent_and_index_from_parent[current_child] = (current, i)
